0e4e94a1946d307d09e46d2d7ad1bb5c6c51693e
Son Huynh <huynhngocsonuit2000github@gmail.com>
2026-04-20 23:26:35 +0700

    Initialize layout + Basic layout for Public and Main

--- app/frontend/fakebook-ui/src/app/app.routes.ts
+++ app/frontend/fakebook-ui/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PublicLayout } from './layout/public-layout/public-layout';
+import { MainLayout } from './layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //   Public page
+  {
+    path: '',
+    component: PublicLayout,
+    children: [{ path: '', loadComponent: () => import('./pages/home/home').then((m) => m.Home) }],
+  },
+
+  // Main pages, after the user logged in, will be here, with main layout
+  {
+    path: 'feed',
+    component: MainLayout,
+    children: [{ path: '', loadComponent: () => import('./pages/feed/feed').then((m) => m.Feed) }],
+  },
+];
